3b98a44a10b4d3280fcb5b168bc5b676524ee71f
clementfly <clement_fly@yahoo.com.tw>
2016-05-30 17:32:18 +0800

    //Test Remark2

--- src/com/mkyong/controller/BaseController.java
+++ src/com/mkyong/controller/BaseController.java
@@ -14,7 +14,7 @@ public class BaseController {
 	private static int counter = 0;
 	private static final String VIEW_INDEX = "index";
 	private final static org.slf4j.Logger logger = LoggerFactory.getLogger(BaseController.class);
-
+	//Test Remark2
 	//Branch update
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String welcome(ModelMap model) {
